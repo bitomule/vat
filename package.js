1197450fb798607fc95fa75bb761dc2081081daf
David Collado Sela <bitomule@gmail.com>
2015-08-04 13:59:56 +0200

    name error

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "bitomule:var",
+  name: "bitomule:vat",
   summary: "Meteor package that exports Europe vat % object",
   version: "0.1.0",
   git: "https://github.com/bitomule/vat"
